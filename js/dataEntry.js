28b29e68b31d13451651c92e06a0eb17b5f59eb8
Arian Ebrahimi <itsarianebrahimi@gmail.com>
2021-01-19 02:15:31 +0330

    Connect Data to HTML and functionalize dataEntry.js :)

--- js/dataEntry.js
+++ js/dataEntry.js
@@ -1,11 +1,12 @@
-var Data={
-    title:"AerBir Resume",
-    bio:"What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?\n",
-    name:"Arian Ebrahimi"
+function setViaId(id  , data){
+    document.getElementById(id).innerHTML=data;
 }
-document.title=Data["title"];
-document.getElementById("bio").innerHTML=Data["bio"];
-document.getElementById("name").innerHTML=Data["name"];
-
-
+function setTittle(title){
+    document.title=title;
+}
+function HomeText(data) {
+        setTittle(data[0].title);
+        setViaId("bio",data[0].bio);
+        setViaId("name",data[0].name);
+}
 
