524759bf25061f65fb0afad66e2730d1beb8b459
Arian Ebrahimi <itsarianebrahimi@gmail.com>
2021-01-19 01:29:41 +0330

    Separate JavaScript and HTML

---
+++ js/dataEntry.js
@@ -0,0 +1,11 @@
+var Data={
+    title:"AerBir Resume",
+    bio:"What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?\n",
+    name:"Arian Ebrahimi"
+}
+document.title=Data["title"];
+document.getElementById("bio").innerHTML=Data["bio"];
+document.getElementById("name").innerHTML=Data["name"];
+
+
+
