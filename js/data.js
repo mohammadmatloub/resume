f3f4004f47bfb51234da024938cb6207cc4b7043
Arian Ebrahimi <itsarianebrahimi@gmail.com>
2021-01-25 01:47:47 +0330

    Adding fontawsome font icon

--- js/data.js
+++ js/data.js
@@ -1,3 +1,5 @@
+
+
 HomeText([
     {
         "title":"AerBir Resume",
@@ -7,6 +9,69 @@ HomeText([
 	    "bgImg":"img/bg.jpg",
     }
 ]);
-Links([{
-        "GitHub":""
-}]);
+
+Skill([
+    {
+        "percent":80,
+        "icon":"fab fa-git-alt",
+        "title":"git",
+
+    },
+    {
+        "percent":95,
+        "icon":"fab fa-html5",
+        "title":"Html",
+
+    },
+    {
+        "percent":90,
+        "icon":"fab fa-css3",
+        "title":"Css",
+
+    },
+    {
+        "percent":90,
+        "icon":"fab fa-js",
+        "title":"Java script",
+
+    },
+    {
+        "percent":68,
+        "icon":"fab fa-linux",
+        "title":"GNU/Linux",
+
+    },
+    {
+        "percent":100,
+        "icon":"fab fa-php",
+        "title":"PHP",
+
+    },
+    {
+        "percent":55,
+        "icon":"fa fa-network-wired",
+        "title":"Networking",
+    },
+
+]);
+
+Links([
+    {
+        "href":"tel:+989102625250",
+        "icon":"fas fa-mobile",
+    },
+    {
+        "href":"https://github.com/ribrea",
+        "icon":"fab fa-github",
+    },
+    {
+        "href":"https://www.linkedin.com/in/arian-ebrahimi-7a85781bb/",
+        "icon":"fab fa-linkedin",
+    },
+    {
+        "href":"mailto:itsarianebrahimi@gmail.com",
+        "icon":"fab fa-google",
+    },
+
+
+]);
--- js/dataEntry.js
+++ js/dataEntry.js
@@ -31,11 +31,51 @@ function _setAttr(des , attrName , attrVal){
 function _setBgImg(bgImgSrc){
     document.body.style.backgroundImage = "url("+bgImgSrc+")";
 }
-function HomeText(data) {
-        _setTittle(data[0].title);
-        _setInner("#bio",data[0].bio);
-        _setInner("#name",data[0].name);
-        _setAttr("#profilePic" , 'src' ,data[0].profilePic);
-        _setBgImg(data[0].bgImg);
-}
 
+
+
+
+
+function HomeText(homeData) {
+        _setTittle(homeData[0].title);
+        _setInner("#bio",homeData[0].bio);
+        _setInner("#name",homeData[0].name);
+        _setAttr("#profilePic" , 'src' ,homeData[0].profilePic);
+        _setBgImg(homeData[0].bgImg);
+}
+function Links(linksData) {
+        let x=Object.keys(linksData).length;
+        let i;
+        for ( i=0 ; i < x; i++){
+            let _elementA = document.createElement("a");
+            let _elementI = document.createElement("i");
+            _elementA.setAttribute("href",linksData[i].href);
+            _elementA.setAttribute("id", "headerSocialLinkA"+i);
+            _elementA.setAttribute("class", "headerSocialLinkA");
+            _elementA.setAttribute("target", "_blank");
+            _elementI.setAttribute("class",linksData[i].icon+" headerSocialLink");
+            document.getElementById("socialLink").appendChild(_elementA);
+            document.getElementById("headerSocialLinkA"+i).appendChild(_elementI);
+        }
+}
+function Skill(skillData) {
+    let x=Object.keys(skillData).length;
+    for ( i=0 ; i < x; i++){
+        let _elementDiv = document.createElement("div");
+        _elementDiv.setAttribute("class","box skillBox");
+        _elementDiv.setAttribute("id","skill"+i);
+        document.getElementById("skillRow").appendChild(_elementDiv);
+        document.getElementById("skill"+i).innerHTML= "<svg viewbox=\"0 0 100 100\">" +
+        "<path class='skillPath'  stroke-dasharray='"+257.2*skillData[i].percent  /100+",257.2' fill='none' stroke-linecap='round' "+
+        " d='M50 10\n" +
+        "a 40 40 0 0 1 0 80\n" +
+        "40 40 0 0 1 0 -80\'/>\n" +
+        "<foreignObject class='foreignObject'>" +
+        "<i class='skillIcon "+skillData[i].icon+"'"+"></i>" +
+        "</foreignObject>" +
+        "</svg>"+
+            "<p class='skillName'>"+skillData[i].title+
+        "</p>"
+    }
+
+}
