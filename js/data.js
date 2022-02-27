1dffdc6a6ba3c56eb0e279807cefa576f3587e4e
pouyababaie <pouyababaie123@gmail.com>
2022-02-27 19:30:21 +0330

    major update on data

--- js/data.js
+++ js/data.js
@@ -1,94 +1,91 @@
 HomeText([
   {
-    title: "Mohammad Matloub",
-    name: "Mohammad Matloub",
-    profilePic: "img/profile_pic.jpg",
-    email: "mohammadmatloub20001379@gmail.com",
-    phone: "+989386351132",
-    object: " ",
+    title: 'Mohammad Matloub',
+    name: 'Mohammad Matloub',
+    profilePic: 'img/profile_pic.jpg',
+    email: 'mohammadmatloub20001379@gmail.com',
+    phone: '+989386351132',
+    object: ' ',
   },
 ]);
 Links([
   {
-    href: "https://github.com/mohammadmatloub",
-    icon: "fab fa-github",
+    href: 'https://github.com/mohammadmatloub',
+    icon: 'fab fa-github',
   },
   {
-    href: "https://www.linkedin.com/in/mohammad-matloub-4756011a5/",
-    icon: "fab fa-linkedin",
+    href: 'https://www.linkedin.com/in/mohammad-matloub-4756011a5/',
+    icon: 'fab fa-linkedin',
   },
   {
-    href: "pdf/cv.pdf",
-    icon: "fa fa-file-pdf",
+    href: 'pdf/cv.pdf',
+    icon: 'fa fa-file-pdf',
   },
 ]);
 Skill([
   {
-    title: "",
+    title: '',
   },
   {
-    title: "",
+    title: '',
   },
   {
-    title: "Git",
+    title: 'Git',
   },
   {
-    title: "",
+    title: '',
   },
   {
-    title: "",
+    title: '',
   },
 ]);
 Language([
   {
-    title: "Persian",
+    title: 'Persian',
   },
   {
-    title: "English",
+    title: 'English',
   },
 ]);
 Framework([
   {
-    title: "",
-    language: "",
+    title: 'Angular',
+    language: 'typescript',
   },
   {
-    title: "",
-    language: "",
+    title: '',
+    language: '',
   },
 ]);
 Experience([
   {
-    title: "",
-    at: "",
-    des: "",
-    startDate: 2020,
-    endDate: 2021,
+    title: 'Angular Developer',
+    at: 'Saam Webware',
+    des: 'As a front-end developer my job is to maintain , update the closed source fin-tech app for compony',
+    startDate: 2019,
+    endDate: 2022,
   },
-  
 ]);
 Education([
   {
-    title: "",
-    study: "",
-    grade: "",
+    title: 'Electrical Engineer',
+    study: 'Azad University at IAUCTB',
+    grade: 'Bachelor',
     endDate: 2024,
   },
 ]);
 Project([
   {
-    title: "",
-    link: "",
-    href: "",
-    des: "",
+    title: '',
+    link: '',
+    href: '',
+    des: '',
   },
-  
 ]);
 Activity([
   {
-    title: "",
-    des: "",
+    title: '',
+    des: '',
   },
- 
 ]);
 Article([]);
